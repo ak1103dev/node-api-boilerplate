ecbed77f500cc57ad43d0fc8ac3a9b878a75afdb
ak1103dev <ak.apichan1103@gmail.com>
2019-07-18 22:58:16 +0700

    :construction: remove unknown key

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -2,7 +2,10 @@ import Joi from '@hapi/joi'
 
 export default schema => (req, res, next) => {
   const data = { ...req.body, ...req.params, ...req.query }
-  const { error, value } = Joi.validate(data, schema, { abortEarly: false })
+  const { error, value } = Joi.validate(data, schema, {
+    abortEarly: false,
+    stripUnknown: true,
+  })
   if (error) {
     res.status(400).send(error)
   } else {
